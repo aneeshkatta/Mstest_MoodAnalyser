3692fb94b8b00d18460ee599c1096fd3ffd29603
Aneesh <katta.aneesh597@gmail.com>
2022-12-10 21:42:55 +0530

    added test case 1.2 of uc-1 program

--- UnitTestProject1/MoodAnalyserTest.cs
+++ UnitTestProject1/MoodAnalyserTest.cs
@@ -6,11 +6,16 @@ namespace UnitTestProject1
 {
     [TestClass]
     public class MoodAnalyserTest
-    {
+    {  //arrange  
         [TestMethod]
-        public void GivenMessageShouldReturnSad()
-        {
-
+        [DataRow("I am in Any Mood","Happy")]//input given
+        public void GivenMessageShouldReturnSad(string messageinput,string expected)
+        {   
+            MoodAnalyser obj = new MoodAnalyser();
+            //act
+            string actual=obj.AnalyseMood(messageinput);
+            //assert
+            Assert.AreEqual(actual, expected);
         }
     }
 }
